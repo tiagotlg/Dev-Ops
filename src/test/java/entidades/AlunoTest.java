1eef46ed8e0ad3beba43ecbc887ae3d9eb2c5571
tiago <tiagotaligon@gmail.com>
2025-09-21 20:26:42 -0300

    Adicionando Test para aluno

---
+++ src/test/java/entidades/AlunoTest.java
@@ -0,0 +1,18 @@
+package entidades;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+class AlunoTest {
+
+    @Test
+    void deveExporNomeERecompensas() {
+        var recompensa = new Recompensa(TipoRecompensa.CRIPTOMOEDA, "Java Avançado");
+        var aluno = new Aluno("João", List.of(recompensa));
+
+        assertEquals("João", aluno.getNome());
+        assertEquals(1, aluno.getRecompensas().size());
+        assertSame(recompensa, aluno.getRecompensas().get(0));
+    }
+}
